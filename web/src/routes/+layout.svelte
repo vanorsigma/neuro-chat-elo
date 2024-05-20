af82625f6d39d05a8a07f5b297756d3392cfcd1a
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2024-05-20 20:49:11 +0100

    Beautify the page a little

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,6 +2,9 @@
   import './page.css';
 </script>
 
-<div class="container mx-auto h-screen w-screen">
-  <slot />
+<div class="container mx-auto flex flex-col justify-center items-center h-screen w-full gap-5">
+  <div class="container flex flex-row justify-center items-center flex-1 max-h-[90%] gap-3">
+    <slot />
+  </div>
+  <p class="flex-none">Public Alpha. Please ping @vanorsigma for feedback</p>
 </div>
