8be20756ee338d14d3385819458c010da179ba31
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2024-05-20 11:49:43 +0100

    Update placeholders

--- web/src/lib/ranks.ts
+++ web/src/lib/ranks.ts
@@ -3,18 +3,21 @@ import { readable } from 'svelte/store';
 // TODO: Currently dummy data
 export const ranks = readable([{
   rank: 1,
-  elo: 100.2,
+  elo: 200.2,
   username: 'hjalnir',
-  delta: 1.0
+  delta: 1.0,
+  avatar: 'https://static-cdn.jtvnw.net/jtv_user_pictures/90e5b4cf-72ce-42f4-9685-a5794bfec28d-profile_image-300x300.png'
 },
 {
   rank: 2,
-  elo: 200.2,
+  elo: 150,
   username: 'cjmaxik',
-  delta: 1.0
+  delta: 1.0,
+  avatar: 'https://static-cdn.jtvnw.net/jtv_user_pictures/d75d49ea-af2d-410b-9c63-2f8dcb341122-profile_image-300x300.png'
 }, ...Array.from({ length: 100 }, (v, i) => ({
-  rank: i + 2,
-  elo: 100,
-  username: `user ${i + 2}`,
-  delta: 1.0
+  rank: i + 3,
+  elo: 100 - i,
+  username: `user ${i + 3}`,
+  delta: 1.0,
+  avatar: 'https://i.pinimg.com/474x/b8/10/b7/b810b717e748149f5b8a39daabff88a4.jpg'
 }))], () => {});
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,6 +2,8 @@
   import Leaderboard from '$lib/leaderboard.svelte';
   import Podium from '$lib/podium.svelte';
   import { ranks } from '$lib/ranks';
+  import type { User } from '$lib/user';
+  import { onMount } from 'svelte';
 
   // TODO: Placeholder function
   async function getRankPage(curr: string, direction: boolean) {
@@ -16,13 +18,29 @@
             `${indexToFetch + offset}`,
             shouldFetchMore];
   }
+
+  async function getTopUsers(): Promise<User[]> {
+    return $ranks.slice().sort((a, b) => a.rank > b.rank).slice(0, 3).map((data) => ({
+      name: data.username,
+      elo: data.elo,
+      avatar: data.avatar,
+    }));
+  }
+
+  let topUsers: User[] | undefined;
+
+  onMount(async () => {
+    topUsers = await getTopUsers();
+  });
 </script>
 
 <h1 class="text-3xl">Podium</h1>
-<Podium />
+{#if topUsers}
+  <Podium firstPlace={topUsers[0]} secondPlace={topUsers[1]} thirdPlace={topUsers[2]} />
+{/if}
 
-<!-- <h1 class="text-3xl">Leaderboard #1</h1> -->
-<!-- <Leaderboard fetchNextPage={getRankPage} /> -->
+<h1 class="text-3xl">Leaderboard #1</h1>
+<Leaderboard fetchNextPage={getRankPage} />
 
 <!-- <h1 class="text-3xl">Leaderboard #2</h1> -->
 <!-- <Leaderboard fetchNextPage={getRankPage} /> -->
