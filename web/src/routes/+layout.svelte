804b4113ca4988b572d2d7f1a5a5460af03a5bd7
James <lim.en.hui.james@gmail.com>
2024-06-02 15:15:39 +0100

    Mobile styling issues

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,9 +2,13 @@
   import './page.css';
 </script>
 
-<div class="container mx-auto flex flex-col justify-center items-center h-screen w-full gap-5">
-  <div class="container h-full md:max-h-[90%]">
+<div
+  class="container mx-auto flex flex-col justify-center items-center h-screen w-full gap-1 md:gap-5"
+>
+  <div class="container h-full md:max-h-[90%] pb-2 sm:pb-0">
     <slot />
   </div>
-  <p class="flex-none text-center">Public Alpha. Please ping @vanorsigma for feedback</p>
+  <p class="flex-none text-center hidden sm:block">
+    Public Alpha. Please ping @vanorsigma for feedback
+  </p>
 </div>
