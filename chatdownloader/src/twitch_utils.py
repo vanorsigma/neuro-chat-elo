e78b6ace2184a910e1f7bf2778a3a70331cc1ace
James <lim.en.hui.james@gmail.com>
2024-06-02 18:52:53 +0100

    Asyncify

--- chatdownloader/src/twitch_utils.py
+++ chatdownloader/src/twitch_utils.py
@@ -11,13 +11,15 @@ TWITCH_APPID = os.getenv('TWITCH_APPID')
 TWITCH_APPSECRET = os.getenv('TWITCH_APPSECRET')
 
 
-def get_auth_twitch() -> Twitch:
-    return Twitch(TWITCH_APPID, TWITCH_APPSECRET)
+async def get_auth_twitch() -> Twitch:
+    return await Twitch(TWITCH_APPID, TWITCH_APPSECRET)
 
 
-def get_latest_vod(twitch: Twitch, ch_id: str) -> str:
+async def get_latest_vod(twitch: Twitch, ch_id: str) -> str:
     logging.info('Getting latest VOD')
-    videos = list(twitch.get_videos(user_id=ch_id, first=1))
+    videos = []
+    async for vid in twitch.get_videos(user_id=ch_id, first=1):
+        videos.append(vid)
     assert len(videos) > 0
     logging.info('Will get chat for VOD %s', videos[0].id)
     return videos[0].id
