d860aae4c96cfe89ffef8b7c613fe9a99839e26f
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2024-06-02 18:52:53 +0100

    Asyncify

--- chatdownloader/src/main.py
+++ chatdownloader/src/main.py
@@ -4,6 +4,8 @@ Script entrypoint
 
 import logging
 
+import asyncio
+
 from twitch_utils import get_auth_twitch, get_latest_vod
 from chatlogprocessor import ChatLogProcessor
 from twitchdownloaderproxy import TwitchChatDownloader
@@ -16,9 +18,13 @@ VED_CH_ID = '85498365'
 if __name__ !='__main__':
     print('This script is meant to be run independently')
 
+async def get_video_id():
+    twitch = await get_auth_twitch()
+    return await get_latest_vod(twitch, VED_CH_ID)
+
 logging.info('Script triggered, pulling latest Twitch VOD')
-video_id = get_latest_vod(get_auth_twitch(), VED_CH_ID)
-logging.info('Pulling chat logs')
+video_id = asyncio.run(get_video_id())
+logging.info('Pulling chat logs for %s', video_id)
 
 with TwitchChatDownloader() as tdp:
     retrieved_chatlogs = tdp.download_chat(video_id)
