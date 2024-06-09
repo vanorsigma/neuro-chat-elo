1f2c5efa95616f41974e1b4e82e22d2103a76f63
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2024-06-09 19:31:28 +0100

    Add yippie and make mobile friendly

--- web/src/lib/revealCard.svelte
+++ web/src/lib/revealCard.svelte
@@ -1,45 +1,50 @@
 <script lang="ts">
-  import { onMount, tick } from 'svelte';
+  import { onMount } from 'svelte';
   import canvasConfetti from 'canvas-confetti';
+  import { fade } from 'svelte/transition';
   let canvas: HTMLCanvasElement;
   let confettiCanvas: HTMLCanvasElement;
   let offscreen: OffscreenCanvas;
 
+  let drumAudio: HTMLAudioElement;
+  let yippieAudio: HTMLAudioElement;
+
+  let titleHide = false;
   let offscreenDone = false;
 
-  // TODO: These are placeholders
-  const AVATAR_URL =
+  let maxAvatarWidth = NaN;
+  const textSize = 30;
+
+  export let avatar_url =
     'https://64.media.tumblr.com/d55402ac7df5b658290fca647b1d4300/fa59626f4dee859b-14/s1280x1920/b86af79b7490871b668274dde83532bfc81d434b.jpg';
-  const AVATAR_WIDTH = 200;
-  const AVATAR_HEIGHT = 200;
-  const NAME = 'test';
+  export let avatar_name = 'vanorsigma';
 
   function drawConfetti(canvas: HTMLCanvasElement) {
     canvasConfetti.create(canvas, {
       resize: true
     })({
-      particleCount: 100,
+      particleCount: 300,
       spread: 160,
       origin: { x: 0.5, y: 0.5 }
     });
   }
 
-  function drawOffscreen(parent: CanvasRenderingContext2D): Promise<void> {
+  function drawOffscreen(): Promise<OffscreenCanvasRenderingContext2D> {
     const ctx = offscreen.getContext('2d');
     if (!ctx) {
       throw new Error('2d context not supported');
     }
 
     const img = new Image();
-    img.src = AVATAR_URL;
+    img.src = avatar_url;
     return new Promise((resolve) => {
       img.onload = () => {
-        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, AVATAR_WIDTH, AVATAR_HEIGHT);
-        ctx.font = '30px Arial';
-        ctx.fillText(NAME, 10, 50);
-        parent.drawImage(offscreen, 0, 0);
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, maxAvatarWidth, maxAvatarWidth);
+        ctx.textAlign = 'center';
+        ctx.font = '30px arial bold';
+        ctx.fillText(avatar_name, maxAvatarWidth * 0.5, maxAvatarWidth + textSize / 2 + 10);
         offscreenDone = true;
-        resolve();
+        resolve(ctx);
       };
     });
   }
@@ -50,13 +55,43 @@
       throw new Error('2d context not supported');
     }
 
-    offscreen = new OffscreenCanvas(200, 200);
-    await drawOffscreen(ctx);
-    drawConfetti(confettiCanvas);
+    maxAvatarWidth = canvas.clientWidth;
+    canvas.width = maxAvatarWidth;
+    canvas.height = maxAvatarWidth + textSize / 2 + textSize;
+
+    offscreen = new OffscreenCanvas(maxAvatarWidth, maxAvatarWidth + textSize / 2 + textSize);
+    await drawOffscreen();
+    drumAudio.play();
+    setTimeout(() => {
+      titleHide = true;
+      setTimeout(() => {
+        yippieAudio.play();
+        ctx.drawImage(offscreen, 0, 0);
+        drawConfetti(confettiCanvas);
+      }, 500);
+    }, 4000);
   });
 </script>
 
 <div class="flex container h-full items-center justify-center">
-  <canvas bind:this={canvas} class="absolute" style="width: 200px; height: 200px"></canvas>
-  <canvas bind:this={confettiCanvas} class="absolute" style="width: 400px; height: 400px"></canvas>
+  {#if !offscreenDone}
+    <p>Loading...</p>
+  {/if}
+
+  {#if offscreenDone && !titleHide}
+    <p transition:fade={{ duration: 200 }} class="text-4xl">Overall Top Chatter is...</p>
+  {/if}
+
+  <canvas
+    bind:this={canvas}
+    in:fade={{ duration: 200, delay: 1000 }}
+    class="absolute w-[60%] md:w-[20%] {offscreenDone ? '' : 'invisible'}"
+  ></canvas>
+  <canvas
+    bind:this={confettiCanvas}
+    in:fade={{ duration: 200, delay: 200 }}
+    class="absolute w-full h-full {offscreenDone ? '' : 'invisible'}"
+  ></canvas>
+  <audio bind:this={yippieAudio} autoplay={false} muted={false} src="./audio/yippie.mp3"></audio>
+  <audio bind:this={drumAudio} autoplay={false} muted={false} src="./audio/drum.mp3"></audio>
 </div>
