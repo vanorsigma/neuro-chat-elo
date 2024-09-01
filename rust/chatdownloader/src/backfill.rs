a14fd1bd0bdc3f0fdc848a9a8b8176c4c636250c
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2024-09-01 16:00:43 +0100

    Update backfill to do Discord

--- rust/chatdownloader/src/backfill.rs
+++ rust/chatdownloader/src/backfill.rs
@@ -2,13 +2,16 @@
 A function to backfill given video IDs
 */
 
+use std::env;
 use std::sync::Arc;
 
+use elo::_types::clptypes::Message;
 use log::info;
 use twitch_utils::seventvclient::SevenTVClient;
 
 use crate::chatlogprocessor::ChatLogProcessor;
 use crate::twitchdownloaderproxy::TwitchChatDownloader;
+use crate::{discorddownloaderproxy, CHANNEL_ID};
 use twitch_utils::TwitchAPIWrapper;
 
 pub async fn backfill() {
@@ -25,11 +28,35 @@ pub async fn backfill() {
         let chat_log = downloader
             .download_chat(video_id)
             .await
-            .expect("Could not download chat log: {e:?}");
+            .expect("Could not download chat log: {e:?}")
+            .comments
+            .into_iter()
+            .map(Message::Twitch);
+
+        let discord_messages = match env::var("CHAT_DISCORD_TOKEN") {
+            Ok(token) => {
+                let (start_time, end_time) = twitch.get_vod_times(video_id.to_string()).await;
+                discorddownloaderproxy::DiscordChatDownloader::new()
+                    .download_chat(
+                        start_time.into(),
+                        end_time.into(),
+                        CHANNEL_ID,
+                        token.as_str(),
+                    )
+                    .await
+                    .expect("Failed to download Discord chat")
+                    .messages
+            }
+            _ => {
+                vec![]
+            }
+        }
+        .into_iter()
+        .map(|m| Message::Discord(m));
 
         let user_performances = ChatLogProcessor::new(&twitch, seventv_client.clone())
             .await
-            .process_from_log_object(chat_log)
+            .process_from_messages(chat_log.chain(discord_messages))
             .await;
 
         ChatLogProcessor::export_to_leaderboards(user_performances).await;
