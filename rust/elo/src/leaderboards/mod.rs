63a3a8d15f7caab12bcf8664ea69c336f25d85dc
jameshi16 <1856293+jameshi16@users.noreply.github.com>
2024-09-22 09:54:54 +0100

    Fix leaderboard async issues

--- rust/elo/src/leaderboards/mod.rs
+++ rust/elo/src/leaderboards/mod.rs
@@ -1,17 +1,17 @@
+mod adventures_farm;
 mod bilibililivestreamchat;
 mod bitsonly;
+mod casual_pxls;
 mod chatonly;
 mod copypastaleaders;
 mod discordlivestreamchat;
+mod ironmouse_pxls;
 mod leaderboardtrait;
 mod nonvips;
 mod overall;
 mod partnersonly;
 mod subsonly;
-mod adventures_farm;
 mod topemote;
-mod casual_pxls;
-mod ironmouse_pxls;
 
 use futures::join;
 
@@ -22,56 +22,87 @@ use crate::{
     _types::clptypes::UserChatPerformance, leaderboards::leaderboardtrait::AbstractLeaderboard,
 };
 
-async fn calc_leaderboard<M: AbstractLeaderboard + Sync + Send + 'static>(
-    leaderboard: &mut M,
-    mut reciever: broadcast::Receiver<UserChatPerformance>,
+async fn calc_leaderboard<L: AbstractLeaderboard + Sync + Send + 'static>(
+    leaderboard: &mut WithReceiver<L>,
 ) {
     /*
     Update the leaderboard based on chat messages sent by a tokio broadcast channel
     */
     loop {
-        let user_chat_performance: UserChatPerformance = match reciever.recv().await {
+        let user_chat_performance: UserChatPerformance = match leaderboard.receiver.recv().await {
             Ok(user_chat_performance) => user_chat_performance,
             Err(_) => break,
         };
-        leaderboard.update_leaderboard(user_chat_performance);
+        leaderboard
+            .leaderboard
+            .update_leaderboard(user_chat_performance);
     }
-    leaderboard.save();
+    leaderboard.leaderboard.save();
+}
+
+struct WithReceiver<L: AbstractLeaderboard> {
+    pub leaderboard: L,
+    pub receiver: broadcast::Receiver<UserChatPerformance>,
+}
+
+impl<L: AbstractLeaderboard> WithReceiver<L> {
+    fn new(leaderboard: L, receiver: broadcast::Receiver<UserChatPerformance>) -> Self
+    where
+        Self: Sized,
+    {
+        Self {
+            leaderboard,
+            receiver,
+        }
+    }
 }
 
 pub struct LeaderboardProcessor {
-    bitsonly: bitsonly::BitsOnly,
-    chatonly: chatonly::ChatOnly,
-    copypasta: copypastaleaders::CopypastaLeaders,
-    nonvips: nonvips::NonVIPS,
-    overall: overall::Overall,
-    subsonly: subsonly::SubsOnly,
-    topemote: topemote::TopEmote,
-    discordlivestreamchat: discordlivestreamchat::DiscordLivestreamChat,
-    partnersonly: partnersonly::PartnersOnly,
-    bilibililivestreamchat: bilibililivestreamchat::BilibiliLivestreamChat,
-    adventures_farm: adventures_farm::AdventuresFarm,
-    casual_pxls: casual_pxls::CasualPxls,
-    ironmouse_pxls: ironmouse_pxls::IronmousePxls
+    tx: Option<broadcast::Sender<UserChatPerformance>>,
+    bitsonly: WithReceiver<bitsonly::BitsOnly>,
+    chatonly: WithReceiver<chatonly::ChatOnly>,
+    copypasta: WithReceiver<copypastaleaders::CopypastaLeaders>,
+    nonvips: WithReceiver<nonvips::NonVIPS>,
+    overall: WithReceiver<overall::Overall>,
+    subsonly: WithReceiver<subsonly::SubsOnly>,
+    topemote: WithReceiver<topemote::TopEmote>,
+    discordlivestreamchat: WithReceiver<discordlivestreamchat::DiscordLivestreamChat>,
+    partnersonly: WithReceiver<partnersonly::PartnersOnly>,
+    bilibililivestreamchat: WithReceiver<bilibililivestreamchat::BilibiliLivestreamChat>,
+    adventures_farm: WithReceiver<adventures_farm::AdventuresFarm>,
+    casual_pxls: WithReceiver<casual_pxls::CasualPxls>,
+    ironmouse_pxls: WithReceiver<ironmouse_pxls::IronmousePxls>,
 }
 
 impl LeaderboardProcessor {
     pub fn new() -> Self {
-        let bitsonly = bitsonly::BitsOnly::new();
-        let chatonly = chatonly::ChatOnly::new();
-        let copypasta = copypastaleaders::CopypastaLeaders::new();
-        let nonvips = nonvips::NonVIPS::new();
-        let overall = overall::Overall::new();
-        let subsonly = subsonly::SubsOnly::new();
-        let topemote = topemote::TopEmote::new();
-        let discordlivestreamchat = discordlivestreamchat::DiscordLivestreamChat::new();
-        let partnersonly = partnersonly::PartnersOnly::new();
-        let bilibililivestreamchat = bilibililivestreamchat::BilibiliLivestreamChat::new();
-        let adventures_farm = adventures_farm::AdventuresFarm::new();
-        let casual_pxls = casual_pxls::CasualPxls::new();
-        let ironmouse_pxls = ironmouse_pxls::IronmousePxls::new();
+        let (tx, rx) = broadcast::channel(100000);
+
+        let bitsonly = WithReceiver::new(bitsonly::BitsOnly::new(), rx.resubscribe());
+        let chatonly = WithReceiver::new(chatonly::ChatOnly::new(), rx.resubscribe());
+        let copypasta =
+            WithReceiver::new(copypastaleaders::CopypastaLeaders::new(), rx.resubscribe());
+        let nonvips = WithReceiver::new(nonvips::NonVIPS::new(), rx.resubscribe());
+        let overall = WithReceiver::new(overall::Overall::new(), rx.resubscribe());
+        let subsonly = WithReceiver::new(subsonly::SubsOnly::new(), rx.resubscribe());
+        let topemote = WithReceiver::new(topemote::TopEmote::new(), rx.resubscribe());
+        let discordlivestreamchat = WithReceiver::new(
+            discordlivestreamchat::DiscordLivestreamChat::new(),
+            rx.resubscribe(),
+        );
+        let partnersonly = WithReceiver::new(partnersonly::PartnersOnly::new(), rx.resubscribe());
+        let bilibililivestreamchat = WithReceiver::new(
+            bilibililivestreamchat::BilibiliLivestreamChat::new(),
+            rx.resubscribe(),
+        );
+        let adventures_farm =
+            WithReceiver::new(adventures_farm::AdventuresFarm::new(), rx.resubscribe());
+        let casual_pxls = WithReceiver::new(casual_pxls::CasualPxls::new(), rx.resubscribe());
+        let ironmouse_pxls =
+            WithReceiver::new(ironmouse_pxls::IronmousePxls::new(), rx.resubscribe());
 
         Self {
+            tx: Some(tx),
             bitsonly,
             chatonly,
             copypasta,
@@ -84,54 +115,52 @@ impl LeaderboardProcessor {
             bilibililivestreamchat,
             adventures_farm,
             casual_pxls,
-            ironmouse_pxls
+            ironmouse_pxls,
         }
     }
 
     pub async fn run(&mut self, performances: Vec<UserChatPerformance>) {
-        let (broadcast_sender, broadcast_reciever) = broadcast::channel(100000);
-
         join!(
-            send_performances(broadcast_sender, performances),
-            calc_leaderboard(&mut self.bitsonly, broadcast_reciever.resubscribe()),
-            calc_leaderboard(&mut self.chatonly, broadcast_reciever.resubscribe()),
-            calc_leaderboard(&mut self.copypasta, broadcast_reciever.resubscribe()),
-            calc_leaderboard(&mut self.nonvips, broadcast_reciever.resubscribe()),
-            calc_leaderboard(&mut self.overall, broadcast_reciever.resubscribe()),
-            calc_leaderboard(&mut self.subsonly, broadcast_reciever.resubscribe()),
-            calc_leaderboard(&mut self.topemote, broadcast_reciever.resubscribe()),
+            send_performances(&mut self.tx, performances),
+            calc_leaderboard(&mut self.bitsonly),
+            calc_leaderboard(&mut self.chatonly),
+            calc_leaderboard(&mut self.copypasta),
+            calc_leaderboard(&mut self.nonvips),
+            calc_leaderboard(&mut self.overall),
+            calc_leaderboard(&mut self.subsonly),
+            calc_leaderboard(&mut self.topemote),
             calc_leaderboard(
-                &mut self.discordlivestreamchat,
-                broadcast_reciever.resubscribe()
+                &mut self.discordlivestreamchat
             ),
-            calc_leaderboard(&mut self.partnersonly, broadcast_reciever.resubscribe()),
+            calc_leaderboard(&mut self.partnersonly),
             calc_leaderboard(
                 &mut self.bilibililivestreamchat,
-                broadcast_reciever.resubscribe()
             ),
             calc_leaderboard(
                 &mut self.adventures_farm,
-                broadcast_reciever.resubscribe()
             ),
             calc_leaderboard(
                 &mut self.casual_pxls,
-                broadcast_reciever.resubscribe()
             ),
             calc_leaderboard(
                 &mut self.ironmouse_pxls,
-                broadcast_reciever.resubscribe()
             ),
         );
     }
 }
 
 pub async fn send_performances(
-    sender: broadcast::Sender<UserChatPerformance>,
+    sender: &mut Option<broadcast::Sender<UserChatPerformance>>,
     performances: Vec<UserChatPerformance>,
 ) {
-    for performance in performances {
-        if let Err(e) = sender.send(performance) {
-            error!("Error sending performance to leaderboards: {}", e);
+    if let Some(tx) = sender {
+        for performance in performances {
+            if let Err(e) = tx.send(performance) {
+                error!("Error sending performance to leaderboards: {}", e);
+            }
         }
+
+        // after the swap, sender will be dropped, which closes all rx
+        std::mem::swap(sender, &mut None);
     }
 }
